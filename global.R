17143fef71f7d767124b049df32682338f4cc8bc
janikmiet <jani.k.miettinen@gmail.com>
2021-02-04 17:40:09 +0200

    I made correction and hilmo timeline now works. Also added progress bar and edited how datasets are wrangled. More updates are coming soon.

--- global.R
+++ global.R
@@ -5,7 +5,6 @@ library(haven)
 library(dplyr)
 ## For shinyapp
 library(flexdashboard)
-library(hrbrthemes)
 library(tidyverse)
 library(Hmisc)
 library(lubridate) 
@@ -13,12 +12,22 @@ library(wordcloud) # maybe not needed
 library(plotly)
 library(survival) # maybe not needed
 library(survminer) # maybe not needed
+library(shiny)
+library(shinydashboard)
 
 ## EDIT THIS PATH! Set a location of prepared dataset imagefile
 load(file="", verbose=T)
 
+## This is needed for shiny app
+minmax_age <- c(min(population$diagnosed_age_yrs), max(population$diagnosed_age_yrs))
+
+## ICD categories
+icd_categories <- c("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "V", "Z", "U")
+
 ## disable scientific number formatting
 options(scipen=999) 
+
+
 
 
 
--- prepare_data.R
+++ prepare_data.R
@@ -1,5 +1,5 @@
-## Data set wrangler script
-## Give datasets and this will produce RData dataset for dataviz application
+## Create datasets for the application
+
 library(haven)
 library(tidyverse)
 library(lubridate)
@@ -16,182 +16,66 @@ input_data_location <- "" ### SET DATA FOLDER LOCATION!
 input_population <- read_sas(paste0(input_data_location, "")) ## SET INPUT DATA NAME!
 input_population$SP <- factor(input_population$SP, labels=c("Male", "Female"))
 
-## Load all ready prepared dataset image file
-# load(file=paste0(nw,"hilmo-visualiser\\dataviz.RData"), verbose=T) 
-
-
-## POPULATION wrangle -----
-
-## !PREPARE:DEFINE THESE VARS!! 
-id <- "idnum"                           # Id number
-birthday_date <- "SYNTPV"               # Date of birth
-diagnosed_date <- "erko307_pv"          # Date when person got diagnosed
-gender_factor <- "SP"                   # Gender in factor var
-died_date <- "kuolpv"                   # Date of person died
-censoring_date <- as.Date("2015-12-31") # Date when follow up study ended
-area_SHP_code <- "SHP"
-
-
-### AUTOMATION
-
-### Harmonize population dataset ###
-population <- input_population %>% 
-  mutate(diagnosed = get(diagnosed_date),
-         diagnosed_age = as.numeric(get(diagnosed_date) - get(birthday_date)) / 365.25,
-         diagnosed_age_yrs = as.numeric(round(diagnosed_age)),
-         diagnosed_yr = year(get(diagnosed_date)),  #  or = format(erko307_pv,"%Y")  # year of diagnosis
-         censored = factor(ifelse(is.na(get(died_date)), 1, 0)),
-         birthday = get(birthday_date),
-         died = get(died_date),
-         survived = ifelse(censored == 1, censoring_date - diagnosed, died - diagnosed), #get(died_date) - diagnosed,
-         id = get(id),
-         gender = get(gender_factor),
-         area_SHP = factor(get(area_SHP_code)), ### TODO CHECK IF OK IN FACTOR
-         survived_yrs_cat = as.character(cut(as.integer(survived),
-                                             breaks = c(-Inf, 180, 365, 730, 1095, Inf),
-                                             labels = c("< half year", "0.5 - 1 year", "1-2 years", "2-3 years", "3+ years"))),
-         survived_yrs_cat_censored = as.factor(ifelse(censored == 0, survived_yrs_cat, "Censored")), ## NOTE: if person is censored before he died, survived category is then "censored"
-         survived_yrs_cat = as.factor(survived_yrs_cat) ## NOTE: if person was alive 2 years after diagnose and at the same time dataset is censored. Person survived 2yrs.
-  ) %>% 
-  select(id, gender, birthday, died, area_SHP, diagnosed, diagnosed_age, diagnosed_age_yrs, diagnosed_yr, survived, survived_yrs_cat, censored) 
-
-### Data check and corrections ###
-## RULE: If ID has diagnosed_day > died -> Recode diagnose day same as died day
-# population <- population[population$survived >= 0, ] # REMOVE
-population[population$survived < 0, "diagnosed"] <- population[population$survived < 0, "died"] # SAME AS DIED
-population[population$survived < 0, "survived"] <- 0 # SAME AS DIED
-
-## Min and max age
-minmax_age <- c(min(population$diagnosed_age_yrs), max(population$diagnosed_age_yrs))
-
-
-## HILMO Wrangle ----
-
 ## !PREPARE: SET THESE DATASET NAME AND VARIABLE NAMES!!!
 ## For Hilmo dataset
 hilmo <- read_sas(paste0(input_data_location, ""))
 
 ## !PREPARE: CHECK IF YOU NEED TO DO THIS WRANGLING!!
 ## Dates, check if you need to do date wrangling!!
-#as.Date(d$tulopv, origin = "1960-01-01") # im not sure if origin date 100% accurate.
 hilmo$tulopv <- as.Date(hilmo$tulopv, origin = "1960-01-01")
 hilmo$lahtopv <- as.Date(hilmo$lahtopv, origin = "1960-01-01")
 hilmo$toimpv <- as.Date(hilmo$toimpv, origin = "1960-01-01")
 
 
-## Creates this kind of dataset
-# year, diagnose, hosp_visit, hosp_days, uniq_ids, ids
+
+## Part  1: population dataset -----
+source("functions/create_population.R")
+population <- create_population(data = input_population)
+
+## Part 2: Create hilmo dataset ----
 hilmo %>% 
   filter(pdgo != "") %>% 
   filter(idnum %in% population$id) %>%  # This because of there some id rows in Hilmo which are not in pop. # hilmo_agg1[is.na(as.integer(hilmo_agg1$tulo_diag)),]
   select(idnum, pdgo, pdge, sdg1o, sdg1e, sdg2o, sdg2e, tulopv, lahtopv) %>% 
   rename(id=idnum) %>% 
   left_join(population %>% select(id, diagnosed)) %>% 
-  mutate(tulo_scaled = as.integer(tulopv - diagnosed),
-         lahto_scaled = as.integer(lahtopv - diagnosed),
-         days = as.integer(lahto_scaled - tulo_scaled + 1),
-         tulo_yr = round(tulo_scaled / 365, 2), # allign with years
-         laht_yr = round(lahto_scaled / 365, 2),
-         pdgo = substr(pdgo,1,3), # 3 digits accurance
-         pdge = substr(pdge,1,3),
-         sdg1o = substr(sdg1o,1,3),
-         sdg1e = substr(sdg1e,1,3),
-         sdg2o = substr(sdg2o,1,3),
-         sdg2e = substr(sdg2e,1,3),
-         source = "hilmo"
-  ) %>% 
-  select(id, pdgo, pdge, sdg1o, sdg1e, sdg2o, sdg2e, tulo_scaled, lahto_scaled, days, tulo_yr, laht_yr, source,  tulopv) %>% 
-  ## RULE: 20 years in timeline before diagnose
-  filter(lahto_scaled > 20 * -365) -> hilmo_agg1
+  mutate(
+    # Scaled enter and leave dates
+    tulo_scaled = as.integer(tulopv - diagnosed),
+    lahto_scaled = as.integer(lahtopv - diagnosed),
+    days = as.integer(lahto_scaled - tulo_scaled + 1),
+    # Scaled enter and leave years
+    tulo_yr = round(tulo_scaled / 365, 2), 
+    laht_yr = round(lahto_scaled / 365, 2),
+    # diagnoses with 3 digits accurance
+    pdgo = substr(pdgo,1,3),
+    pdge = substr(pdge,1,3),
+    sdg1o = substr(sdg1o,1,3),
+    sdg1e = substr(sdg1e,1,3),
+    sdg2o = substr(sdg2o,1,3),
+    sdg2e = substr(sdg2e,1,3)
+  ) -> hilmo_diags
 
-## REMOVE possible duplicates (tulopv included in dataset to make sure only duplicated events are erased)
-hilmo_agg1 <- hilmo_agg1[!duplicated(hilmo_agg1), ]
-hilmo_agg1$ICD <- substr(hilmo_agg1$pdgo, 1, 1)
+#### Main diagnoses
+hilmo_diags %>% 
+  pivot_longer(cols = c("pdgo", "pdge"), names_to = "source", values_to = "main") %>% 
+  filter(main != "") %>% 
+  select(id, source, main, tulo_scaled, lahto_scaled, days, tulo_yr, laht_yr, tulopv) %>% 
+  filter(!duplicated(.)) %>% 
+  mutate(ICD = substr(main, 1, 1)) -> hilmo_maindiags
 
-## Hilmo timeline dataset: 20 year of timeline of pdgo ----
-## TODO add column (list) including id's which are then used in shiny app to filter right population
-## TODO also add to shiny app percentage calculation correcting "the new pop"
-## NOTE: i is end of the time interval. Ex. i=1 means that events that has happened between 0.01 - 1
-## NOTE: calculation is approx because real years are not counted. Year is defined as 365 days.
-hilmo_tl <- NULL
-for(i in seq(from = -20, to = 20, by = 1))for(i in seq(from = -20, to = 20, by = 1)){
-  if(is.null(hilmo_tl)){
-    hilmo_agg1 %>% 
-      filter( ( tulo_yr <= i & laht_yr > i - 1) ) %>% 
-      group_by(ICD) %>% 
-      summarise(
-        time = i,
-        patients = length(unique(id)) ,
-        ## Formula 1.1: This formula is explained in documentation
-        hospital_days = sum(case_when(tulo_yr > (i - 1) & laht_yr <= (i) ~ as.double(days),
-                                      tulo_yr < (i - 1) & laht_yr >= (i) ~ 365,
-                                      tulo_yr < (i - 1) & laht_yr < (i) & laht_yr > (i - 1) ~ abs(abs((i - 1) * 365) - abs(lahto_scaled)),
-                                      tulo_yr > (i - 1) & tulo_yr < (i) & laht_yr > (i + 1) ~ ifelse(i<0, 
-                                                                                                       abs(tulo_scaled) - (abs(i * 365)),
-                                                                                                       abs(abs(i * 365) - abs(tulo_scaled))
-                                      )  
-                                      
-        ), na.rm = T)
-        
-      )   -> hilmo_tl
-  }else{
-    hilmo_tl <- hilmo_tl %>% 
-      rbind(
-        hilmo_agg1 %>% 
-          filter( ( tulo_yr <= i & laht_yr > i - 1) ) %>% 
-          group_by(ICD) %>% 
-          summarise(
-            time = i,
-            patients = length(unique(id)) ,
-            ## Formula 1.1: This formula is explained in documentation
-            hospital_days = sum(case_when(tulo_yr > (i - 1) & laht_yr <= (i) ~ as.double(days),
-                                          tulo_yr < (i - 1) & laht_yr >= (i) ~ 365,
-                                          tulo_yr < (i - 1) & laht_yr < (i) & laht_yr > (i - 1) ~ abs(abs((i - 1) * 365) - abs(lahto_scaled)),
-                                          tulo_yr > (i - 1) & tulo_yr < (i) & laht_yr > (i + 1) ~ ifelse(i<0, 
-                                                                                                           abs(tulo_scaled) - (abs(i * 365)),
-                                                                                                           abs(abs(i * 365) - abs(tulo_scaled))
-                                          )  
-                                          
-            ), na.rm = T)
-            
-          ) 
-      )
-  }
-}
+#### Side diagnoses
+hilmo_diags %>% 
+  pivot_longer(cols = c("sdg1o", "sdg1e", "sdg2o", "sdg2e"), names_to = "source", values_to = "side") %>%
+  filter(side != "") %>% 
+  select(id, source, side, tulo_scaled, lahto_scaled, days, tulo_yr, laht_yr, tulopv) %>% 
+  filter(!duplicated(.)) %>% 
+  mutate(ICD = substr(side, 1, 1)) -> hilmo_sidediags
 
 
-## Check plots
-# ggplot(data = hilmo_tl) +
-#   geom_bar(aes(x = time, y = patients, group = ICD, fill = ICD), stat= "identity")
-# ggplot(data = hilmo_tl) +
-#   geom_bar(aes(x = time, y = hospital_days, group = ICD, fill = ICD), stat= "identity")
-
-
-## Gather diagnose variable to one MAIN / SIDE
-
-## Summarize by year and diagnose
-## Gather information id, 
-
-# ### Hilmo aggregated diagnose info ###
-# hilmo %>% 
-#   filter(pdgo != "") %>% 
-#   mutate(diagnose = substr(pdgo, 1, 3)) %>% 
-#   group_by(diagnose) %>% 
-#   summarise(n=n(),
-#             patients = length(unique(idnum))) -> hilmo_pdgo
-# 
-# hilmo %>% 
-#   filter(pdge != "") %>% 
-#   mutate(diagnose = substr(pdge, 1, 3)) %>% 
-#   group_by(diagnose) %>% 
-#   summarise(n=n(),
-#             patients = length(unique(idnum))) -> hilmo_pdge
-
-
-
-## Clean and save image -----
+## Part 3: Save dataset -----
 
 # Remove temp vars
-rm(list = c("input_population", "id", "birthday_date", "diagnosed_date", "gender_factor", "died_date", "censoring_date", "area_SHP_code", "hilmo"))
+rm(list = c("input_population", "hilmo", "hilmo_timeline", "hilmo_tl", "i"))
 # Save datasets
-save.image(file=paste0(input_data_location, "hilmo-visualiser\\dataviz.RData"))
+save.image(file=paste0(input_data_location, "hilmo-visualiser\\hilmo_datasets.RData"))
